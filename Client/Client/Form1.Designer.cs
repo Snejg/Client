31d95c8e991e4fcd36628f4617f8b11dd9fc6730
Snejg <xdrobny@gmail.com>
2016-02-01 14:40:09 +0100

    Základ - port jako parametr, pripojení k serveru, poslani pozadavku, cekani na odpoved

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_log = new System.Windows.Forms.TextBox();
+            this.textBox_chat = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_log
+            // 
+            this.textBox_log.Location = new System.Drawing.Point(627, 12);
+            this.textBox_log.Multiline = true;
+            this.textBox_log.Name = "textBox_log";
+            this.textBox_log.ReadOnly = true;
+            this.textBox_log.Size = new System.Drawing.Size(153, 444);
+            this.textBox_log.TabIndex = 0;
+            // 
+            // textBox_chat
+            // 
+            this.textBox_chat.Location = new System.Drawing.Point(12, 213);
+            this.textBox_chat.Multiline = true;
+            this.textBox_chat.Name = "textBox_chat";
+            this.textBox_chat.ReadOnly = true;
+            this.textBox_chat.Size = new System.Drawing.Size(609, 180);
+            this.textBox_chat.TabIndex = 1;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(74, 411);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 2;
+            this.btn_send.Text = "Odeslat";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 468);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.textBox_chat);
+            this.Controls.Add(this.textBox_log);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_log;
+        private System.Windows.Forms.TextBox textBox_chat;
+        private System.Windows.Forms.Button btn_send;
+    }
+}
+
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly Socket _clientSocket = new Socket
+        (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private static int _PORT;
+
+        public Form1(int portNumber)
+        {
+            _PORT = portNumber;
+            InitializeComponent();
+        }
+
+        private void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    textBox_log.Text = "Connection attempt " + attempts;
+                    _clientSocket.Connect(IPAddress.Loopback, _PORT);
+                    //_clientSocket.Connect("192.168.1.3", _PORT);
+
+                }
+                catch (SocketException)
+                {
+                    textBox_log.Text = "";
+                }
+            }
+
+            textBox_log.Text = "";
+            textBox_log.AppendText("Connected \n");
+            //backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void RequestLoop()
+        {
+            textBox_log.AppendText(" type exit to properly disconnect client \n");
+
+            while (true)
+            {
+                //SendRequest();
+                //ReceiveResponse();
+            }
+
+        }
+
+        private void Exit()
+        {
+            SendString("exit"); // Tell the server we re exiting
+            _clientSocket.Shutdown(SocketShutdown.Both);
+            _clientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private void SendRequest()
+        {
+            textBox_log.AppendText("Send a request: \n");
+            string request = textBox_chat.Text;
+            SendString(request);
+
+            if (request.ToLower() == "exit")
+            {
+                Exit();
+            }
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding
+        /// </summary>
+        private void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            _clientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = _clientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+
+            this.textBox_log.Invoke(new MethodInvoker(delegate ()
+            { textBox_log.AppendText(text + "\n"); }));
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConnectToServer();
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            SendRequest();
+            ReceiveResponse();
+        }
+    }
+}
